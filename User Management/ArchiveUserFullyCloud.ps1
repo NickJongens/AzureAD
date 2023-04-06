bd13a47e6f226b1c588c690d41a748fb49d619b0
Nick Jongens <nick@jongens.nz>
2023-04-06 15:44:51 +1200

    Initial Upload - Commented actions
    This script is to be used only on fully Microsoft 365 environments. This should not be performed on an Active Directory domain or Azure AD Sync.
    There are a few assumptions including:
    
    - The user will need to be disabled.
    - The user will need to have groups information stored as part of the process 
    - The username will be need to be renamed to include 'ARCHIVED - ' prepended.
    - The user will need to have their mailbox converted to a shared mailbox.
    - The user will need their license/s removed in order to save costs.
    - We need to output the users' groups into a table to send to a client.

---
+++ User Management/ArchiveUserFullyCloud.ps1
@@ -0,0 +1,49 @@
+# Connect to Azure AD and Exchange Online using modern authentication
+Connect-AzureAD
+Connect-MsolService
+$ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
+Import-PSSession $ExchangeSession
+
+# Get the user account to be archived
+$username = Read-Host "Enter the UserPrincipalName of the user to be archived"
+$user = Get-AzureADUser -ObjectId $username
+
+# Disable the user account
+Set-AzureADUser -ObjectId $user.ObjectId -AccountEnabled $false
+
+# Store the user's group memberships
+$groups = Get-AzureADUserMembership -ObjectId $user.ObjectId
+
+# Remove the user from all groups
+foreach ($group in $groups) {
+    Remove-AzureADGroupMember -ObjectId $group.ObjectId -MemberObjectId $user.ObjectId
+}
+
+# Format the user's group memberships in a table
+$table = $groups | Select-Object DisplayName, ObjectId | Format-Table -AutoSize | Out-String
+
+# Rename the user's display name
+$newDisplayName = "ARCHIVED - " + $user.DisplayName
+Set-AzureADUser -ObjectId $user.ObjectId -DisplayName $newDisplayName
+
+# Convert the user's mailbox to a shared mailbox
+Enable-Mailbox -Identity $user.UserPrincipalName -Shared -Confirm:$false
+
+# Remove the user's Office 365 licenses
+$licenses = Get-MsolUserLicense -UserPrincipalName $user.UserPrincipalName
+foreach ($license in $licenses) {
+    $licenseName = $license.AccountSkuId.Replace(":",".")
+    Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -RemoveLicenses $licenseName
+}
+
+# Remove the user account
+Remove-AzureADUser -ObjectId $user.ObjectId
+
+# Disconnect from Azure AD and Exchange Online
+Disconnect-AzureAD
+Disconnect-MsolService
+Remove-PSSession $ExchangeSession
+
+# Output the user's group memberships in a table
+Write-Host "The user was a member of the following groups:" -ForegroundColor Yellow
+Write-Host $table
