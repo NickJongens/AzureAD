e936779393d0b94a06a89f761ddeb8e2d2f32ac0
Nick Jongens <nick@jongens.nz>
2023-04-11 09:30:31 +1200

    Removed Role Gathering as it's duplicated
    Shifted disable of account to the last action

--- User Management/ArchiveUserFullyCloud.ps1
+++ User Management/ArchiveUserFullyCloud.ps1
@@ -7,34 +7,13 @@ Connect-ExchangeOnline
 $username = Read-Host "Enter the UserPrincipalName of the user to be archived"
 $user = Get-AzureADUser -ObjectId $username
 
-# Disable the user account
-Set-AzureADUser -ObjectId $user.ObjectId -AccountEnabled $false
-
-# Store the user's group memberships
-$groups = Get-AzureADUserMembership -ObjectId $user.ObjectId
-
-# Remove the user from all groups
-foreach ($group in $groups) {
-    Remove-AzureADGroupMember -ObjectId $group.ObjectId -MemberObjectId $user.ObjectId
+$groups | ForEach-Object {
+    Write-Host "Removing user '$($user.UserPrincipalName)' from group '$($_.DisplayName)'"
+    Remove-AzureADGroupMember -ObjectId $_.ObjectId -MemberId $user.ObjectId -Confirm:$false
 }
 
 # Format the user's group memberships in a table
 $table = $groups | Select-Object DisplayName, ObjectId | Format-Table -AutoSize | Out-String
-
-# Get the user's directory role memberships
-if ($user.ObjectId) {
-    $roles = Get-AzureADDirectoryRoleMember -ObjectId $user.ObjectId | ForEach-Object {
-        Get-AzureADDirectoryRole -ObjectId $_.DirectoryRole.ObjectId
-    }
-}
-
-# Format the user's roles in a table
-$roleTable = $roles | Select-Object DisplayName, ObjectId | Format-Table -AutoSize | Out-String
-
-# Remove the user from all active directory roles
-foreach ($role in $roles) {
-    Remove-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -MemberObjectId $user.ObjectId
-}
 
 # Rename the user's display name
 $newDisplayName = "ARCHIVED - " + $user.DisplayName
@@ -51,9 +30,9 @@ foreach ($license in $licenses) {
     Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -RemoveLicenses $licenseName
 }
 
+# Disable the user account
+Set-AzureADUser -ObjectId $user.ObjectId -AccountEnabled $false
+
 # Output the user's group memberships in a table
 Write-Host "The user was a member of the following groups:" -ForegroundColor Yellow
 Write-Host $table
-# Output the user's roles in a table
-Write-Host "The user had the following directory roles:" -ForegroundColor Yellow
-Write-Host $roleTable
